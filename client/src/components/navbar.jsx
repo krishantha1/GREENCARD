3470806ebe9d6b4dc9319b66cb090cbae32495f3
Krishantha lakshan <160416327+krishantha1@users.noreply.github.com>
2025-04-17 15:15:45 +0530

    create footer header and the product lists

--- client/src/components/navbar.jsx
+++ client/src/components/navbar.jsx
@@ -1,58 +1,121 @@
 import  React from 'react'
 import { NavLink } from 'react-router-dom'
 import { assets } from '../assets/assets'
+import { useAppContext } from '../context/AppContext'
 
 
 const Navbar = () => 
 {
     const [open, setOpen] = React.useState(false)
+    const {user, setuser,setShowUserLogin,navigate } = useAppContext();
+
+     const Logout = async () => {
+        setuser(null);
+        navigate('/')
+     }
+    
     return (
         <nav className="flex items-center justify-between px-6 md:px-16 lg:px-24 xl:px-32 py-4 border-b border-gray-300 bg-white relative transition-all">
 
-            <NavLink to = '/'>
+            <NavLink to = '/' onClick={ ()=> setOpen(false)}>
                 <img className="h-9" src={assets.logo} alt="logo" />
             </NavLink>
 
             {/* Desktop Menu */}
             <div className="hidden sm:flex items-center gap-8">
-                <NavLink to='/'>Home</NavLink>
-                <NavLink to='/products'>All Product</NavLink>
-                <NavLink to='/'>Contact</NavLink>
+                <NavLink to="/" onClick={() => setOpen(false)}>Home</NavLink>
+                <NavLink to="/products" onClick={()=> setOpen(false)}>All Product</NavLink>
+               {!user && 
+               <NavLink to="/products" onClick={()=> setOpen(false)}>My Orders</NavLink>
+                }
+                <NavLink to="/" onClick={()=> setOpen(false)}>contact</NavLink>
+            
                 <div className="hidden lg:flex items-center text-sm gap-2 border border-gray-300 px-3 rounded-full">
                     <input className="py-1.5 w-full bg-transparent outline-none placeholder-gray-500" type="text" placeholder="Search products" />
-                   <img src ={assets.search_icon} alt=''/>
+                   <img src ={assets.search_icon} alt='search' className='w-4 h-4' />
                 </div>
 
-                <div className="relative cursor-pointer">
-                    <svg width="18" height="18" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M.583.583h2.333l1.564 7.81a1.17 1.17 0 0 0 1.166.94h5.67a1.17 1.17 0 0 0 1.167-.94l.933-4.893H3.5m2.333 8.75a.583.583 0 1 1-1.167 0 .583.583 0 0 1 1.167 0m6.417 0a.583.583 0 1 1-1.167 0 .583.583 0 0 1 1.167 0" stroke="#615fff" stroke-linecap="round" stroke-linejoin="round" />
-                    </svg>
+                <div  onClick={() => navigate("/cart")}  className="relative cursor-pointer">
+                   <img src={assets.nav_cart_icon}  alt= 'cart' className='w-6 opacity-80'/>
                     <button className="absolute -top-2 -right-3 text-xs text-white bg-primary w-[18px] h-[18px] rounded-full">3</button>
                 </div>
 
-                <button className="cursor-pointer px-8 py-2 bg-primary hover:bg-primary-dull transition text-white rounded-full">
-                    Login
-                </button>
-            </div>
 
+                {!user ? (
+
+                   <button  onClick={()=>{
+                
+                    setShowUserLogin(true);
+                   }} className="cursor-pointer px-8 py-2 bg-primary hover:bg-primary-dull transition text-white rounded-full">
+                   Login
+               </button>
+
+                ):(
+                <div className='relative group'>
+                    <img src={assets.profile_icon} className='w-10'/>
+                    <ul className='hidden group-hover:block absolute  top-10 right-0 bg-whitw shadow boder boder-gray-200 py-2.5 w-30 rounded-md text-sm z-40'>
+                        <li onClick={()=> navigate("my-orders")} className='p-1.5 p1-3 hover:bg-primary/10 cursor-pointer'>My Orders</li>
+                        <li onClick={Logout} className='p-1.5 p1-3 hover:bg-primary/10 cursor-pointer'>log Out</li>
+                    </ul>
+                </div>
+                
+                )
+                }
+
+               
+            </div>
+           
             <button onClick={() => open ? setOpen(false) : setOpen(true)} aria-label="Menu" className="sm:hidden">
-                {/* Menu Icon SVG */}
-                <svg width="21" height="15" viewBox="0 0 21 15" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <rect width="21" height="1.5" rx=".75" fill="#426287" />
-                    <rect x="8" y="6" width="13" height="1.5" rx=".75" fill="#426287" />
-                    <rect x="6" y="13" width="15" height="1.5" rx=".75" fill="#426287" />
-                </svg>
+               <img  src={assets.menu_icon}  alt='menu'/>
+               
             </button>
 
+
+
+
+
+
+
+
+
+
             {/* Mobile Menu */}
-            <div className={`${open ? 'flex' : 'hidden'} absolute top-[60px] left-0 w-full bg-white shadow-md py-4 flex-col items-start gap-2 px-5 text-sm md:hidden`}>
-                <a href="#" className="block">Home</a>
-                <a href="#" className="block">About</a>
-                <a href="#" className="block">Contact</a>
-                <button className="cursor-pointer px-6 py-2 mt-2 bg-indigo-500 hover:bg-primary transition text-white rounded-full text-sm">
-                    Login
-                </button>
-            </div>
+            {
+                open &&(
+                <div className={`${open ? 'flex' : 'hidden'} absolute top-[60px] left-0 w-full bg-white shadow-md py-4 flex-col items-start gap-2 px-5 text-sm md:hidden`}>
+                <NavLink to="/"  onClick={()=> setOpen(false)}>Home</NavLink>
+                <NavLink to="/products"  onClick={()=> setOpen(false)}>All Product</NavLink>
+                {user &&
+                <NavLink to="/products"  onClick={()=> setOpen(false)}>My Orders</NavLink>
+                }
+                 <NavLink to="/products"  onClick={()=> setOpen(false)}>contact</NavLink>
+               
+                 {!user ? (
+
+                <button  onClick={()=>{
+                 setOpen(false);
+                 setShowUserLogin(true);
+                }} className="cursor-pointer px-8 py-2 bg-primary hover:bg-primary-dull transition text-white rounded-full">
+               Login
+               </button>
+
+                ):(
+                  <button onClick={Logout} className="cursor-pointer px-8 py-2 bg-primary hover:bg-primary-dull transition text-white rounded-full">
+                LogOut
+                </button>)
+                }
+
+
+                </div>)
+            }
+            
+
+<button onClick={() => open ? setOpen(false) : setOpen(true)} aria-label="Menu" className="sm:hidden">
+<img  src={assets.menu_icon}  alt='menu'/>
+
+</button>
+                
+            
 
         </nav>
     )
--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -1,21 +1,79 @@
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { dummyProducts } from "../assets/assets";
+import toast from "react-hot-toast";
 
 
 // eslint-disable-next-line react-refresh/only-export-components
 export const AppContext =  createContext();
 
 export const AppContextProvider = ({children}) => {
-    
+
+    const  currency = import.meta.VITE_CURRENCY;
     const navigate = useNavigate();
-    const [user, setuser] = useState(null)
+    const [user, setuser] = useState(true)
     const[isSeller,setIsSeller] =useState(false)
+    const[showUserLoging,setShowUserLogin] =useState(false)
+    const[products,setProducts] =useState([])
+    const[cartItems,setCardItems] =useState({})
+
+  const  fetchProducts = async ()=>{
+     setProducts(dummyProducts)
+  }
+
+  // add product to card
+  const addToCart = (itemId)=>{
+    let cartData = structuredClone(cartItems);
+    if(cartData[itemId]){
+     // eslint-disable-next-line no-undef
+     cartData[itemId] += 1;
+    }else{
+        cartData[itemId] = 1;
+    }
+    setCardItems(cartData);
+    toast.success("Added to cart")
+  }
+
+  const updateCartItem = (itemId,quantity)=>{
+    let cartData = structuredClone(cartItems);
+    cartData[itemId] = quantity;
+    setCardItems(cartData)
+    toast.success("card Updated")
+  }
+
+
+// remove product from card
+const removeFromCart =(itemId)=>{
+    let cartData = structuredClone(cartItems);
+    if(cartData[itemId]){
+        cartData[itemId] -= 1;
+        if(cartData[itemId] === 0){
+            delete cartData[itemId];
+        }
+    }
+    toast.success("removed from Cart")
+    setCardItems(cartData)
+
+}
+
+  useEffect(()=>{
+    fetchProducts()
+  },[])
+
     const value = {
         navigate,
         user,
         setuser,
         isSeller,
-        setIsSeller
+        setIsSeller,
+        showUserLoging,
+        setShowUserLogin,
+        products,
+        currency,
+        addToCart,
+        updateCartItem,
+        removeFromCart,
+        cartItems
     }
 
     return <AppContext.Provider  value={value}>
@@ -23,6 +81,7 @@ export const AppContextProvider = ({children}) => {
         </AppContext.Provider>
 }
 
+// eslint-disable-next-line react-refresh/only-export-components
 export const useAppContext = () =>{
     return useContext(AppContext)
 }
